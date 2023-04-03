53ea298d4dfa1a15f85819acd5f986c184a1ee36
Michael Wijaya Sutrisna <michaelwijayasutrisna2002@gmail.com>
2023-04-03 13:47:12 +0700

    Initial Commit

---
+++ MC1App/MC1AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC1AppApp.swift
+//  MC1App
+//
+//  Created by Michael Wijaya Sutrisna on 03/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MC1AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
